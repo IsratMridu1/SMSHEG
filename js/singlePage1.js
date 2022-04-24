a086dbfe5d38526d8f47ba61a207cd6aa85313da
shams_jubair111 <shamsjubairbluebyit@gmail.com>
2022-04-25 02:42:17 +0600

    2 rem

---
+++ js/singlePage1.js
@@ -0,0 +1,17 @@
+const checkPrivacy = (event) => {
+    if(event.checked  == true){
+        document.getElementById('span-1').innerHTML='Please agree to our terms';
+    }
+    else{
+     document.getElementById('span-1').innerHTML='';
+    }
+ }
+ 
+ const checkPrivacy2 = (event) => {
+    if(event.checked  == true){
+        document.getElementById('span-2').innerHTML='';
+    }
+    else{
+     document.getElementById('span-2').innerHTML='Can we call you to get started?';
+    }
+ }
